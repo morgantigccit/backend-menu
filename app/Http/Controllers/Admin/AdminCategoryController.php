3dd046d46fe04e261007693c42fff239e87e8c68
abdelkarimhamd <abdelkarimhamd@gmail.com>
2024-05-05 10:30:18 +0300

    testing for github push and pull requests

--- app/Http/Controllers/Admin/AdminCategoryController.php
+++ app/Http/Controllers/Admin/AdminCategoryController.php
@@ -10,7 +10,7 @@ use Illuminate\Support\Facades\Auth;
 
 class AdminCategoryController extends Controller
 {
-    //testing for github push and pull requests
+    //testing for github push and pull requests testing
     // Display a listing of the categories
     public function index()
     {
